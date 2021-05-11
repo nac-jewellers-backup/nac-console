c2b230125966bab9c5bea7ace31533406511fc9e
vardhman <vardhman@crayond.co>
2021-05-11 21:36:06 +0530

    updated product_sync

---
+++ build/precache-manifest.acf4ad5cdbdf8b4f712778ae7a2e1e04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b2379a62d2ca88bf67da73b0b0a875",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17051606c95e125a55e6",
+    "url": "/static/css/2.5a4d534d.chunk.css"
+  },
+  {
+    "revision": "bd369b95b859fc4af5d8",
+    "url": "/static/css/main.c69f60e7.chunk.css"
+  },
+  {
+    "revision": "17051606c95e125a55e6",
+    "url": "/static/js/2.127d6ed1.chunk.js"
+  },
+  {
+    "revision": "bd369b95b859fc4af5d8",
+    "url": "/static/js/main.becb1123.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
