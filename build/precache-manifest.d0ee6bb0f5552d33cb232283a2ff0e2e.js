a8081b01ded0d332415389d5fd6c5e94d09396dd
Samir <samirsaji13@gmail.com>
2021-06-21 12:02:58 +0530

    build and pushed

---
+++ build/precache-manifest.d0ee6bb0f5552d33cb232283a2ff0e2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e840b99086b72597963ea9220685e91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8972ab664fe4ddb35c65",
+    "url": "/static/css/2.d8e743e4.chunk.css"
+  },
+  {
+    "revision": "4b59c1ee3ac8c0141602",
+    "url": "/static/css/main.c69f60e7.chunk.css"
+  },
+  {
+    "revision": "8972ab664fe4ddb35c65",
+    "url": "/static/js/2.1a26f6a3.chunk.js"
+  },
+  {
+    "revision": "4b59c1ee3ac8c0141602",
+    "url": "/static/js/main.61bbbd0d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
