b28dcffb7de67658e9bad64d107f8b5f65663baa
vardhman <vardhman@crayond.co>
2021-07-13 10:12:29 +0530

    fixed markups

---
+++ build/precache-manifest.7d7cc48b4b4b28320ca2be8ffda5809f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce1ac55c94f3a7d4c773cb6f462cb7dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbe432150b2059d3c83e",
+    "url": "/static/css/2.d8e743e4.chunk.css"
+  },
+  {
+    "revision": "17c3120efce8242e873a",
+    "url": "/static/css/main.c69f60e7.chunk.css"
+  },
+  {
+    "revision": "fbe432150b2059d3c83e",
+    "url": "/static/js/2.94acf2e5.chunk.js"
+  },
+  {
+    "revision": "17c3120efce8242e873a",
+    "url": "/static/js/main.e31d2397.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
