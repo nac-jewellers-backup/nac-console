94bc82bbcb101f97d0f976810c8eb0ef2f464b24
vishnuvarthan <vishnuvarthan@crayond.co>
2022-10-07 15:23:37 +0530

    new changes

---
+++ build/precache-manifest.50b36d70a994f247ae1b9a809e8078b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800b0348f2d560f67c9515410965a15a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48d531ba8339cc63df2d",
+    "url": "/static/css/2.6b064f09.chunk.css"
+  },
+  {
+    "revision": "a98d0e43703fd7a0baff",
+    "url": "/static/css/main.91300c0e.chunk.css"
+  },
+  {
+    "revision": "48d531ba8339cc63df2d",
+    "url": "/static/js/2.b5d37de8.chunk.js"
+  },
+  {
+    "revision": "a98d0e43703fd7a0baff",
+    "url": "/static/js/main.a132ae4a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
