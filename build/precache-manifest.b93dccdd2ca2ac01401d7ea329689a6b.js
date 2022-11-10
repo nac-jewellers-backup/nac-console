dca39a560fad42d3d75744a1d39550fa5fa47afe
renalsalex <renals@cryond.co>
2022-11-10 13:08:47 +0530

    CMS 1st phase completed and Akshaya Tritiya. commited by Renals. 1:08PM, 10/11/2022

---
+++ build/precache-manifest.b93dccdd2ca2ac01401d7ea329689a6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd9408bf18bbadd5a9d07dfb354b676a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e268b1ba7878f9c8d6e9",
+    "url": "/static/css/2.6b064f09.chunk.css"
+  },
+  {
+    "revision": "6af55b0e84f8aad3fe43",
+    "url": "/static/css/main.cba23928.chunk.css"
+  },
+  {
+    "revision": "e268b1ba7878f9c8d6e9",
+    "url": "/static/js/2.ef4e773f.chunk.js"
+  },
+  {
+    "revision": "6af55b0e84f8aad3fe43",
+    "url": "/static/js/main.44a3363f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
