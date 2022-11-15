f58c0b6708b3f6a86b9748638f77656652390a57
vishnuvarthan <vishnuvarthan@crayond.co>
2022-11-15 19:08:08 +0530

    appointment changes

---
+++ build/precache-manifest.b9b5335863756eed66fe6e971c766728.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4528489e6ca7e6ed4674d9d968842ac6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6ef1a429629491c07be",
+    "url": "/static/css/2.6b064f09.chunk.css"
+  },
+  {
+    "revision": "21465a5b6bf313d31a18",
+    "url": "/static/css/main.91300c0e.chunk.css"
+  },
+  {
+    "revision": "b6ef1a429629491c07be",
+    "url": "/static/js/2.85b19292.chunk.js"
+  },
+  {
+    "revision": "21465a5b6bf313d31a18",
+    "url": "/static/js/main.753c5f16.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
