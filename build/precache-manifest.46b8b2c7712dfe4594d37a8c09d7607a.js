20c33024e0c39e8fa93b4b128a92e9c856b37e98
renalsalex <renals@cryond.co>
2022-11-22 11:34:55 +0530

    Bridal collection CMS page completed and it is moved to the staging.Commited by Renals. On 22/11/2022. 11:34AM

---
+++ build/precache-manifest.46b8b2c7712dfe4594d37a8c09d7607a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afc422a16992f768b945f2dc3c16ac6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65f2372be2da43fa47d8",
+    "url": "/static/css/2.56787ea2.chunk.css"
+  },
+  {
+    "revision": "f4a83c7ef88d53b8a6a0",
+    "url": "/static/css/main.2b5995e4.chunk.css"
+  },
+  {
+    "revision": "65f2372be2da43fa47d8",
+    "url": "/static/js/2.4787e874.chunk.js"
+  },
+  {
+    "revision": "f4a83c7ef88d53b8a6a0",
+    "url": "/static/js/main.52dfd579.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
