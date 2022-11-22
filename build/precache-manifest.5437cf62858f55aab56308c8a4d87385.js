2253bfaf45dd680d530b87e9964708962ac12043
renalsalex <renals@cryond.co>
2022-11-22 14:01:58 +0530

    News room cms page has been completed and to be moved to staging.Commited by renals. 22/11/2022, 2:01PM

---
+++ build/precache-manifest.5437cf62858f55aab56308c8a4d87385.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b39c854d38ae6f9396a9f2e6839e7ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4cfdb189c51deda84e6",
+    "url": "/static/css/2.56787ea2.chunk.css"
+  },
+  {
+    "revision": "8f2bed2465a2cccf4794",
+    "url": "/static/css/main.2b5995e4.chunk.css"
+  },
+  {
+    "revision": "c4cfdb189c51deda84e6",
+    "url": "/static/js/2.3ab81be7.chunk.js"
+  },
+  {
+    "revision": "8f2bed2465a2cccf4794",
+    "url": "/static/js/main.1b43477a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
