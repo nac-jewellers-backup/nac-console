84738eae323ea901b275fcafab25fb5a59d6f770
renalsalex <renals@cryond.co>
2022-11-23 12:57:42 +0530

    Collection Page CMS has been completed and the Home page names has been changed.Commited by Renals. On 23/11/2022. At 12:57PM

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5504e4be101937064a46fe157f531230.js"
+  "/precache-manifest.fbe09b969406f78cb65e51a6939dd680.js"
 );
 
 self.addEventListener('message', (event) => {
