f9aa5572913801aa23be381fff53a32f9b99405a
renalsalex <renals@cryond.co>
2022-12-22 19:20:09 +0530

    CMS has been done without the Temple Work Page. Committed by Renals. On 22/12/2022. at 7:20PM

---
+++ build/precache-manifest.a5e5b9a27a94c4d4d64c35d9ebf24348.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "908fdbfcac5d676c4acfc41ee1df8f5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de341c7d3029a50f5894",
+    "url": "/static/css/2.4afb56ee.chunk.css"
+  },
+  {
+    "revision": "9ad351ee1bc7d81adc00",
+    "url": "/static/css/main.2b5995e4.chunk.css"
+  },
+  {
+    "revision": "de341c7d3029a50f5894",
+    "url": "/static/js/2.2938f98e.chunk.js"
+  },
+  {
+    "revision": "9ad351ee1bc7d81adc00",
+    "url": "/static/js/main.1551c4ed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
