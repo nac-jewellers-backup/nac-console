69ea89aca86cf5397d01530646a961cfb723b8cd
renalsalex <renals@cryond.co>
2023-01-27 12:33:38 +0530

    CMS complete pages move to production on the console.Comitted by renals on 27/1/2023 at 12:30PM

---
+++ build/precache-manifest.ccc621a73d93fc77013f367fc4ec6176.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3dadb8837a68027a6156fb8c2651dfe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60603993e696aa48650c",
+    "url": "/static/css/2.4afb56ee.chunk.css"
+  },
+  {
+    "revision": "6524e8c4b938cd1f9dbe",
+    "url": "/static/css/main.2b5995e4.chunk.css"
+  },
+  {
+    "revision": "60603993e696aa48650c",
+    "url": "/static/js/2.3936270d.chunk.js"
+  },
+  {
+    "revision": "6524e8c4b938cd1f9dbe",
+    "url": "/static/js/main.045f92a4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
