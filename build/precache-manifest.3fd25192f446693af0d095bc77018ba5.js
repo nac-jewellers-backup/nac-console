be5b5686b0b3bdbb5772616e5f83ae7d57507fef
Renals Alex <renals@crayond.co>
2023-03-08 17:52:50 +0530

    changes in the banner error fixed. Committed by renals on 8/3/23 at 5:52PM

---
+++ build/precache-manifest.3fd25192f446693af0d095bc77018ba5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40fc72dd1be1da948ef185f0219126a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60603993e696aa48650c",
+    "url": "/static/css/2.4afb56ee.chunk.css"
+  },
+  {
+    "revision": "d194da9e8eabe56ef891",
+    "url": "/static/css/main.2b5995e4.chunk.css"
+  },
+  {
+    "revision": "60603993e696aa48650c",
+    "url": "/static/js/2.3936270d.chunk.js"
+  },
+  {
+    "revision": "d194da9e8eabe56ef891",
+    "url": "/static/js/main.d276d0ad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
